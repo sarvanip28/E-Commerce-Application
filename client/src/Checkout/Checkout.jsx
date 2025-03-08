7542f7b4161ecdf21179593e4837f6bd9cd275b1
sarvanip28 <sarvanipidikiti123@gmail.com>
2025-03-08 15:58:25 +0530

    backend api updated

--- client/src/Checkout/Checkout.jsx
+++ client/src/Checkout/Checkout.jsx
@@ -34,7 +34,7 @@ const Checkout = () => {
             }
 
             try {
-                const response = await axios.post("http://localhost:5000/api/payment/create-payment-intent", {
+                const response = await axios.post("https://e-commerce-application-o9uz.onrender.com/api/payment/create-payment-intent", {
                     amount: totalPrice * 100,
                     customerName,
                     customerEmail,
